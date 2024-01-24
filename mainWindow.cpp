790d757015a516fac0ef9881a5b660dddcc4f0ef
Yannick <yannick.zhang@etu.sorbonne-universite.fr>
2024-01-24 18:31:24 +0100

    infoPage

--- mainWindow.cpp
+++ mainWindow.cpp
@@ -2,12 +2,21 @@
 #include "mainWindow.hpp"
 #include "ui_mainWindow.h"
 #include "detailedWindow.hpp"
+#include "dataManager.hpp"
+#include "epreuve.hpp"
 
-MainWindow::MainWindow(QWidget *parent)
-    : QMainWindow(parent)
-    , ui(new Ui::MainWindow)
+MainWindow::MainWindow(QWidget *parent) : QMainWindow(parent), ui(new Ui::MainWindow)
 {
+    // Configuration de l'attribut ui (User Interface)
     ui->setupUi(this);
+
+    // Configuration des attributs listeEpreuves et listeRestaurants à partir du fichier JSON
+    DataManager dataManager(":/lieux.json");
+    listeEpreuves = dataManager.toListEpreuves();
+    listeRestaurants = dataManager.toListRestaurants();
+
+    // Configuration de l'attribut scrollAreaLayout où seront ajoutés les widgets des épreuves
+    scrollAreaLayout = new QVBoxLayout(ui->scrollAreaWidgetContents);
 
     // Connecter les signaux aux slots
     connect(ui->openInfoMainButton, SIGNAL(clicked()), this, SLOT(openInfoMain()));
@@ -22,12 +31,13 @@ MainWindow::~MainWindow()
 
 void MainWindow::openInfoMain()
 {
-    ui->stackedWidget->setCurrentIndex(0);
+    ui->stackedWidget->setCurrentIndex(1);
+    setupEpreuveScrollArea(listeEpreuves);
 }
 
 void MainWindow::openMapMain()
 {
-    ui->stackedWidget->setCurrentIndex(1);
+    ui->stackedWidget->setCurrentIndex(2);
 }
 
 //void MainWindow::openDetailedInfo()
@@ -53,5 +63,52 @@ void MainWindow::openDetailedInfo()
         detailedWindow.setModal(true);
         detailedWindow.setDetails(details); // Passez les détails à la fenêtre de dialogue
         detailedWindow.exec();
+    }
+}
+
+
+void MainWindow::addEpreuveScrollArea(const Epreuve &epreuve)
+{
+    // Créer un widget pour chaque épreuve 
+    QWidget *epreuveWidget = new QWidget();
+    QVBoxLayout *epreuveLayout = new QVBoxLayout(); // épreuve
+    QHBoxLayout *scrollLayout = new QHBoxLayout();  // image + nom + horaire
+
+    // Image à gauche de epreuveLayout
+    QLabel *imageLabel = new QLabel;
+    imageLabel->setPixmap(QPixmap(":/images/basket.jpg").scaled(50, 50));
+    scrollLayout->addWidget(imageLabel);
+
+    // Informations à droite de epreuveLayout
+    QVBoxLayout *infoLayout = new QVBoxLayout();
+    QLabel *nomLabel = new QLabel(epreuve.getNom());
+    QLabel *horaireLabel = new QLabel(epreuve.getHoraireDebut().toString());
+        // Ajustements de la taille et de l'alignement des labels
+    for (auto infoLabel : {nomLabel, horaireLabel})
+    {
+        infoLabel->setFixedWidth(300);
+        infoLabel->setAlignment(Qt::AlignVCenter | Qt::AlignLeft);
+        infoLabel->setSizePolicy(QSizePolicy::Expanding, QSizePolicy::Preferred);
+        infoLayout->addWidget(infoLabel);
+    }
+
+    // Ajoutez le QVBoxLayout (nom et horaire) à droite du QHBoxLayout
+    scrollLayout->addLayout(infoLayout);
+
+    // Ajoutez le QHBoxLayout au QVBoxLayout principal de l'épreuve
+    epreuveLayout->addLayout(scrollLayout);
+
+    // Configurez le widget de l'épreuve et ajoutez-le au layout principal de la scroll area
+    epreuveWidget->setLayout(epreuveLayout);
+    scrollAreaLayout->addWidget(epreuveWidget);
+}
+
+
+
+void MainWindow::setupEpreuveScrollArea(QVector<Epreuve> listeEpreuves)
+{
+    for (const Epreuve &epreuve : listeEpreuves)
+    {
+        addEpreuveScrollArea(epreuve);
     }
 }
--- mainWindow.hpp
+++ mainWindow.hpp
@@ -2,13 +2,16 @@
 #pragma once
 
 #include "ui_mainWindow.h"
+#include "detailedWindow.hpp"
+#include "dataManager.hpp"
+#include "epreuve.hpp"
+#include "restaurant.hpp"
 
 #include <QMainWindow>
 #include <QScrollArea>
 #include <QLabel>
 #include <QVBoxLayout>
 #include <QDialog>
-
 QT_BEGIN_NAMESPACE
 namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
@@ -30,5 +33,11 @@ private slots:
 
 private:
     Ui::MainWindow *ui;
-};
+    QVector<Epreuve> listeEpreuves;
+    QVector<Restaurant> listeRestaurants;
+    QVBoxLayout *scrollAreaLayout;
 
+    // Méthodes pour configurer les résultats de la barre de recherche
+    void addEpreuveScrollArea(const Epreuve &epreuve);
+    void setupEpreuveScrollArea(QVector<Epreuve> listeEpreuves);
+};
